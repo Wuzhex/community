b00164799f538160456054520d8e9e8951f67d29
wuze <595358277@qq.com>
2019-10-14 11:43:39 +0800

    init repo

---
+++ src/main/java/life/learn/wuze/WuzeApplication.java
@@ -0,0 +1,13 @@
+package life.learn.wuze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuzeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuzeApplication.class, args);
+    }
+
+}
